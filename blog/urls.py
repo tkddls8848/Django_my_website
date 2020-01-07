c80972c988bf8e22656ec5dd72e1fe254d7eb71a
tkddls8848 <tkddls8848@gmail.com>
2020-01-07 17:59:47 +0900

    Django PostList View

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
 
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+'''
 def index(request):
     posts = Post.objects.all()
     return render(
@@ -12,3 +17,4 @@ def index(request):
             "posts" : posts
         }
     )
+'''
