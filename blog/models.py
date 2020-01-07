8a554173803ec430155348b77b6e277b35ddea28
tkddls8848 <tkddls8848@gmail.com>
2020-01-07 14:20:31 +0900

    blog title format

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,5 @@ class Post(models.Model):
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
+    def __str__(self):
+        return '{} :: {}'.format(self.title, self.author)
