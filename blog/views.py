46b61604c16c438b14e0220ff0e4bc0d84359863
tkddls8848 <tkddls8848@gmail.com>
2020-01-12 16:45:11 +0900

    make Post Detail page.

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,13 @@
-from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
 
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
         return Post.objects.order_by('-created')
-'''
-def index(request):
-    posts = Post.objects.all()
-    return render(
-        request,
-        'blog/index.html',
-        {
-            "posts" : posts
-        }
-    )
-'''
+
+
+class PostDetail(DetailView):
+    model = Post
